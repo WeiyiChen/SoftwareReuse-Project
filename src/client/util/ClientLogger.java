7226e41cb45c0ee9560f590cc80857560f85c63c
Dai Dongyang <daidongyang12@gmail.com>
2016-03-29 02:02:09 +0800

    modify ClientLogger.java

--- src/client/util/ClientLogger.java
+++ src/client/util/ClientLogger.java
@@ -5,11 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 
 public class ClientLogger {
 	private static boolean isConnect;
 	private static boolean isLogin;
-	private static String logFileName;
+	private static String logFileName = "log.txt";
 	private static int sendNum;
 	private static int receiveNum;
 	private static BufferedWriter logWriter;
@@ -62,30 +63,44 @@ public class ClientLogger {
 		return logWriter;
 	}
 	
-	public static void writeLoginSuccessful() throws IOException{
-		
-		if(logWriter == null){
-			buildLogWriter();
-		}
-		logWriter.write("login success!\n");
+	public static void writeLoginSuccessful(String usr) throws IOException{
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("log.txt", true)));
+		out.write(usr + " login successfully!\n");
+		out.flush();
+		out.close();
+		updateUsr(usr);
+		logWriter.write(new Date().toString() + "\n");
+		logWriter.write("login successfully!\n");
 		logWriter.flush();
 	}
 	
-	public static void writeLoginFailed() throws IOException{
-		if(logWriter == null){
-			buildLogWriter();
-		}
-		logWriter.write("login fail!\n");
-		logWriter.flush();
+	public static void writeLoginFailed(String usr) throws IOException{
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("log.txt", true)));
+		out.write(usr + " login failed!\n");
+		out.flush();
+		out.close();
 	}
 	
 	public static void writePerMinute() throws IOException{
+		if(!isLogin){
+			return;
+		}
 		if(logWriter == null){
 			buildLogWriter();
 		}
+		logWriter.write(new Date().toString() + "\n");
 		logWriter.write("send message : " + sendNum + "\n");
 		logWriter.write("receive message : " + receiveNum + "\n");
 		logWriter.flush();
+	}
+	
+	public static void closeLogWriter() throws IOException{
+		if(logWriter != null){
+			logWriter.close();
+			logWriter = null;
+		}
 	}
 	
 }
