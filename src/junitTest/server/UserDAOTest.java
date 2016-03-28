0e9f6bad2cd250dc0874ec6621492731975a6266
WeiyiChen <gintama93629@gmail.com>
2016-03-28 12:43:41 +0800

    add unit test of UserDAO

---
+++ src/junitTest/server/UserDAOTest.java
@@ -0,0 +1,40 @@
+package junitTest.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.DAO.UserDAO;
+
+public class UserDAOTest {
+
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUserDAO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		String str = new UserDAO().getPassword("qyd");
+		System.out.println(str);
+	}
+
+	@Test
+	public void testAddUser() {
+		UserDAO dao =new UserDAO();
+		System.out.println(dao.addUser("jcy", "shjdyxy"));
+
+	}
+
+	@Test
+	public void testCheckOrCreateJsonFile() {
+		//new UserDAO().checkOrCreateJsonFile();
+	}
+
+}
