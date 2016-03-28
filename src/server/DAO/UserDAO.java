3bb1df3742c0df57f961d31d2b77a7d439a81f54
WeiyiChen <gintama93629@gmail.com>
2016-03-28 12:12:03 +0800

    Refactor DAO

--- src/server/DAO/UserDAO.java
+++ src/server/DAO/UserDAO.java
@@ -1,53 +1,111 @@
-package server.DAO;
+ipackage server.DAO;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
-import org.json.parser.ParseException;
-import org.json.parser.JSONParser;
+import org.json.JSONString;
+import org.json.JSONStringer;
+
 
 public class UserDAO {
-	public  static JSONArray jsonDatabase = new JSONArray();
+	private String dirPath = "./data";
+	private String filePath = "./data/users.json";
+	
+	private Map<String, String> userMap = new HashMap<String, String>();
+		
+	public UserDAO(){
+		checkOrCreateJsonFile();
+		getUsersAndPassword();
+	}
+	
+	@Override
+	public void finalize(){
+		saveUsers();
+	}
 	
 	public String getPassword(String user) {
-		for(int i = 0; i < jsonDatabase.length(); i ++){
-			JSONObject jsonObj = jsonDataBase.getJSONObject(i);
-			String jsonUser = jsonObj.getString("User");
-			if(jsonUser == user){
-				String password = jsonObj.getString("Password");
+		String pwd = userMap.get(user);
+		if(pwd == null){
+			pwd = "";
+		}
+		return pwd;
+	}
+	
+	public boolean addUser(String user, String password) {
+		if(userMap.containsKey(user)){
+			return false;
+		}
+		userMap.put(user, password);
+		saveUsers();
+		return true;
+	}
+	
+	private String getBasicUserString() {
+		String basicUserString = "";
+		try {
+			JSONStringer js = new JSONStringer();
+			basicUserString = js.object().key("qyd").value("1252865")
+					.key("cwy").value("1252874").endObject().toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return basicUserString;
+	}
+	
+	private void getUsersAndPassword(){
+		String jsonString = FileAccess.readFile(filePath);
+		if(jsonString.equals("")){
+			return;
+		}
+		try {
+			JSONObject jsonObj = new JSONObject(jsonString);
+			@SuppressWarnings("unchecked")
+			Iterator<String> it = jsonObj.keys();
+			userMap.clear();
+			while(it.hasNext()){
+				String key = it.next();
+				userMap.put(key, jsonObj.getString(key));
 			}
-			return password;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void checkOrCreateJsonFile(){
+		
+		try {
+			File dataFolder = new File(dirPath);
+			if (!dataFolder.exists() || !dataFolder.isDirectory()) {
+				dataFolder.mkdir();
+			}
+			File dataFile = new File(filePath);
+			if(!dataFile.exists() || !dataFile.isFile()){
+				dataFile.createNewFile();
+				FileAccess.fileOverWrite(filePath, getBasicUserString());
+			}
+		} catch (Exception e) {
+
 		}
 		
-	}
-	
-	public void addUser(String user, String password) {
 		
 	}
 	
-	public void creatJsonDatabase(){
-		JSONParser parser = new JSONParser();
-		try{
-			FileInputStream jsonFile = new FileInputStream("/Users/chenweiyi/Desktop/jsonDatabase");
-			StringBuffer txtContent = new StringBuffer();
-			String line;
-			while((line = jsonFile.read()) != -1){
-				txtContent.append(line);
-			}
-			jsonFile.close();
-			Object obj = parser.parse(line.toString());
-			jsonDatabase = (JSONArray)obj;
-		}catch(ParseException pe){
-			
-	         System.out.println("position: " + pe.getPosition());
-	         System.out.println(pe);
-	    }catch (FileNotFoundException e)
-		{
-	    	e.printStackTrace();
-	    }catch (IOException e)
-	    {
-	    	e.printStackTrace();
-	   	}
-		
+	public boolean saveUsers(){
+		JSONObject jsonObj= new JSONObject(userMap);
+		System.out.println("save:"+jsonObj.toString());
+		return FileAccess.fileOverWrite(filePath, jsonObj.toString());
 	}
-	
 }
