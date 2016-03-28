3dbc4f99a36da6365d0c8f7319fa35fd810e6fea
Dai Dongyang <daidongyang12@gmail.com>
2016-03-28 10:28:21 +0800

    add Config class

--- src/util/ParseConfig.java
+++ src/util/ParseConfig.java
@@ -3,9 +3,8 @@ package util;
 public class ParseConfig {
 	public static void writeFile(){}
 	
-	//result[0] ip;
-	//resutl[1] port number;
-	public static String[] readIpAndPort(){
+	
+	public static Config readIpAndPort(){
 		return null;
 	}
 }
