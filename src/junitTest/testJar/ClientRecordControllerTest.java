b2cb81a4c501d1be80a4f32285907587433b26f3
Dai Dongyang <daidongyang12@gmail.com>
2016-04-13 00:32:07 +0800

    test record-1.0.jar

---
+++ src/junitTest/testJar/ClientRecordControllerTest.java
@@ -0,0 +1,31 @@
+package junitTest.testJar;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import teamEleven.record.ClientRecordController;
+
+public class ClientRecordControllerTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		//fail("Not yet implemented");
+		ClientRecordController crc = ClientRecordController.getInstance();
+		crc.setUser("hello");
+		crc.setAndStart(1);
+		for(int i = 0; i < 100; i++){
+			TimeUnit.MILLISECONDS.sleep(50);
+			crc.receiveNumAdd();
+			crc.sendNumAdd();
+			crc.loginFailedCountAdd();
+			crc.loginSucceedCountAdd();
+		}
+		crc.reset();
+		TimeUnit.SECONDS.sleep(2);
+		System.out.println("end");
+	}
+
+}
