d278e1e2596a15e1b29e7993194b7b7644091940
Dai Dongyang <daidongyang12@gmail.com>
2016-04-19 15:58:49 +0800

    stage after reset

---
+++ src/server/ctrl/ServerConfigBean.java
@@ -0,0 +1,25 @@
+package server.ctrl;
+
+public class ServerConfigBean {
+	private int maxMessagesPerLogin;
+	private int maxMessagesPerSecond;
+	private int saveCycle;
+	public int getMaxMessagesPerLogin() {
+		return maxMessagesPerLogin;
+	}
+	public void setMaxMessagesPerLogin(int maxMessagesPerLogin) {
+		this.maxMessagesPerLogin = maxMessagesPerLogin;
+	}
+	public int getMaxMessagesPerSecond() {
+		return maxMessagesPerSecond;
+	}
+	public void setMaxMessagesPerSecond(int maxMessagesPerSecond) {
+		this.maxMessagesPerSecond = maxMessagesPerSecond;
+	}
+	public int getSaveCycle() {
+		return saveCycle;
+	}
+	public void setSaveCycle(int saveCycle) {
+		this.saveCycle = saveCycle;
+	}
+}
