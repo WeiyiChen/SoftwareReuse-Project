720f7bb93daadb25df73c621a651d851ec5b7c73
sbrkl <ruankangle123@126.com>
2016-04-22 11:09:29 +0800

    client receive msg save to txt without path config

--- src/client/transport/Client11SReciever.java
+++ src/client/transport/Client11SReciever.java
@@ -1,9 +1,11 @@
 package client.transport;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import client.intf.IMsgHandle;
 
@@ -11,6 +13,7 @@ public class Client11SReciever extends Thread {
 	private static boolean isExit = false;
 	private Socket socket;
 	private IMsgHandle imh;
+	private ArrayList<String> clientReceiveMsg = new ArrayList<String>();
 
 	public static void exit(){
 		isExit = true;
@@ -32,7 +35,9 @@ public class Client11SReciever extends Thread {
 			String message = null;
 			while (true) {
 				message = reader.readLine();
-//				System.out.println(message);
+				//System.out.println(message);
+				clientReceiveMsg.add(message);
+				saveClientMsgToFile(clientReceiveMsg);
 				if(imh != null){
 					imh.receiveAndUpdateMsg(message);
 				}
@@ -58,5 +63,14 @@ public class Client11SReciever extends Thread {
 //				e.printStackTrace();
 //			}
 		}
+	}
+	
+	private void saveClientMsgToFile(ArrayList<String> msg) throws IOException {
+	    FileWriter writer = new FileWriter("client_msg.txt"); 
+        for(String str: msg) {
+          writer.write(str);
+        }
+        writer.close();
+	    
 	}
 }
