215be540ee5e34d1e69bd552c865375eca4d8981
WeiyiChen <gintama93629@gmial.com>
2016-04-26 22:04:48 +0800

    update ziplogcontroller

--- src/server/ctrl/ZipLogController.java
+++ src/server/ctrl/ZipLogController.java
@@ -1,5 +1,50 @@
 package server.ctrl;
 
+import java.io.IOException;
+
+import packedZip.Zip;
+
 public class ZipLogController {
 
+	private class ZipRecordThread extends Thread {
+
+		private boolean continueFlag;
+		private String originLogForder;
+		private String zipedLogZip;
+		
+		public ZipRecordThread(String originLogForder, String zipedLogFile) {
+			super();
+			this.originLogForder = originLogForder;
+			this.zipedLogZip = zipedLogFile;
+			continueFlag = true;
+		}
+
+		public void setStop() {
+			continueFlag = false;
+		}
+
+		@Override
+		public void run() {
+			while (continueFlag) {
+				do {
+					try {
+						int i = 86400;
+						while (continueFlag && i > 0) {
+							Thread.sleep(1000);
+							i--;
+						}
+						Zip.zip(this.originLogForder,this.zipedLogZip);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						
+					}
+					//rec.save();
+					
+				} while (continueFlag);
+			}
+			System.out.println("record thread stop");
+		}
+
+	}
 }
