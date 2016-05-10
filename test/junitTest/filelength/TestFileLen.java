2da461cd2738dc0714b08cf0dc1c187c7d969424
Dai Dongyang <daidongyang12@gmail.com>
2016-05-10 20:16:02 +0800

    test Outputstream.write(int i)

--- test/junitTest/filelength/TestFileLen.java
+++ test/junitTest/filelength/TestFileLen.java
@@ -1,5 +1,6 @@
 package junitTest.filelength;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -7,7 +8,25 @@ import java.io.PrintWriter;
 import org.junit.Test;
 
 public class TestFileLen {
+	
 	@Test
+	public void testWriteb(){
+		int i = 1 + 2 + 2*2 + 2*2*2 + 2*2*2*2 + 2*2*2*2*2 + 2*2*2*2*2*2;
+		i = 2*i + 1;
+		i = 2*i + 1;
+		i = 2*i + 1;
+		System.out.println(i);
+		System.out.println(Integer.toBinaryString(i));
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(10);
+		byte[] bs = baos.toByteArray();
+		System.out.println(bs.length);
+		baos.write(i);
+		bs = baos.toByteArray();
+		System.out.println(bs.length);
+		printByteArray(baos.toByteArray());
+	}
+	
+//	@Test
 	public void testFileLen() throws FileNotFoundException{
 //		File file = new File("/Users/d/Desktop/");
 //		System.out.println(file.length());
@@ -21,5 +40,13 @@ public class TestFileLen {
 		}
 		pw.close();
 		System.out.println(file.length());
+	}
+	
+	public void printByteArray(byte[] bs){
+		System.out.println(bs.length);
+		for(byte b : bs){
+			System.out.print(b);
+		}
+		System.out.println();
 	}
 }
