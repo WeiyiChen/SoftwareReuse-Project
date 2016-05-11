b8c0a3bb41a770a70f135b876f0fc1b9ea0d4d42
Dai Dongyang <daidongyang12@gmail.com>
2016-05-11 10:57:10 +0800

    update 程序文档

--- rls/team/eleven/file/limit/FilesLimitSizeOutputStream.java
+++ rls/team/eleven/file/limit/FilesLimitSizeOutputStream.java
@@ -14,6 +14,7 @@ import java.io.OutputStream;
  *
  */
 public class FilesLimitSizeOutputStream extends OutputStream {
+	
 	private FileOutputStream fos;
 	private File f;
 	private String preNamePath;
